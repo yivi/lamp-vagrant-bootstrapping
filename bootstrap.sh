925de39fb875bd40140952da7d32e606ba78ee6c
Ivan Yivoff <ivan@yivoff.com>
2015-09-24 19:28:07 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,26 +11,25 @@ read IP CN < <(exec ifconfig en0 | awk '/inet / { t = $2; sub(/.*[.]/, "", t); p
 # sudo mkdir "/var/www/${PROJECTFOLDER}"
 
 # update / upgrade
-sudo apt-get update && sudo apt-get upgrade
+apt-get update && apt-get upgrade
 
 # install apache 2.5 and php 5.5
 echo ">>> installing apache"
-sudo apt-get install -y apache2
+apt-get install -y apache2
 
 echo ">>> installing"
-sudo apt-get install -y php5
+apt-get install -y php5
 
 # install mysql and give password to installer
 echo ">> configuring and install mysql"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server
 
 echo ">>> installing php extensions"
-sudo apt-get install -y php5-curl php5-mcrypt mysql-server php5-xdebug
+apt-get install -y php5-curl php5-mcrypt mysql-server php5-xdebug
 echo ">>> and php-mysql"
 sudo apt-get install -y php5-mysql
-
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
@@ -52,7 +51,7 @@ VHOST=$(cat <<EOF
 </VirtualHost>
 EOF
 )
-sudo echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
 # enable mod_rewrite
 sudo a2enmod rewrite
@@ -61,10 +60,10 @@ sudo a2enmod rewrite
 sudo php5enmod mcrypt
 
 # hoboman runs apache
-sudo perl -pi -e 's/(APACHE_RUN_(USER|GROUP))=www-data/\1=vagrant/g' /etc/apache2/envvars
+perl -pi -e 's/(APACHE_RUN_(USER|GROUP))=www-data/\1=vagrant/g' /etc/apache2/envvars
 
 # no servername complaining
-sudo perl -pi -e 's/(#ServerRoot "\/etc\/apache2")/\1\nServerName local.vagrant/' /etc/apache2/apache2.conf
+perl -pi -e 's/(#ServerRoot "\/etc\/apache2")/\1\nServerName localhost/' /etc/apache2/apache2.conf
 
 XDEBUG_INI=$(cat <<EOF
 zend_extension=xdebug.so
@@ -76,29 +75,29 @@ xdebug.remote_port=9$CN
 EOF
 )
 
-sudo echo "${XDEBUG_INI}" > /etc/php5/mods-available/xdebug.ini
+echo "${XDEBUG_INI}" > /etc/php5/mods-available/xdebug.ini
 
 echo ">>> restarting apache"
-sudo service apache2 restart
+service apache2 restart
 
 # install git
-sudo apt-get -y install git subversion
+apt-get -y install git subversion
 
 # install Composer
 echo ">>> downloading and installing composer"
 curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
 
 echo ">>> installing wp-cli"
 # install wp-cli
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-sudo chmod 755 /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
+chmod 755 /usr/local/bin/wp
 echo ">>> WP-CLI installed"
 
 echo ">>> adjusting locales";
 # locales
-sudo locale-gen es_ES.UTF-8 && sudo dpkg-reconfigure locales
+locale-gen es_ES.UTF-8 && dpkg-reconfigure locales
 
 PHPINFO=$(cat <<EOF
 <?php
@@ -108,5 +107,4 @@ EOF
 
 # nice php info, to have something in place
 echo "${PHPINFO}" > "/var/www/$PROJECT_FOLDER/info.php"
-sudo chown www-data:www-data "/var/www/$PROJECT_FOLDER/info.php" 
-
+chown vagrant: "/var/www/$PROJECT_FOLDER/info.php" 
