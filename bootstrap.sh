823bcb34f344f8ba536b0354b19444fd123bfdaf
Ivan Yivoff <ivan@yivoff.com>
2016-06-25 18:28:57 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,8 @@ DB='MYSQL' # OR 'PXC'
 # ip address last octet of ip address. ipv6, who knew ye?
 read IP CN < <(exec ifconfig eth1 | awk '/inet / { t = $2; sub(/.*[.]/, "", t); print $2, t }')
 
-# create project folder (only necessary if we are not syncing yet, so maybe not?)
-# sudo mkdir "/var/www/${PROJECTFOLDER}"
+# boostrapping apt-get with  update / upgrade
+apt-get update && apt-get upgrade
 
 # better hostname
 echo $PROJECTFOLDER | tee /etc/hostname
@@ -25,14 +25,15 @@ export LANG=es_ES.UTF-8
 export LC_ALL=es_ES.UTF-8
 dpkg-reconfigure locales
 
-# update / upgrade
-apt-get update && apt-get upgrade
-
 echo "Europe/Madrid" | tee /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
+echo ">>> downloading adminer"
+mkdir -p /var/www/adminer & chown vagrant: /var/www/adminer
+wget --quiet http://www.adminer.org/latest.php -O /var/www/adminer/index.php
+
 if [ $HTTPD = 'APACHE' ]; then
-	# install apache 2.5
+	# install apache 2.4
 	echo ">>> installing apache"
 	apt-get install -y apache2
 
@@ -42,24 +43,61 @@ if [ $HTTPD = 'APACHE' ]; then
 	# no servername complaining, svp
 	perl -pi -e 's/(#ServerRoot "\/etc\/apache2")/\1\nServerName localhost/' /etc/apache2/apache2.conf
 
-	# setup hosts file
+	# setup hosts file(s)
 	VHOST=$(cat <<EOF
 	<VirtualHost *:80>
 	    DocumentRoot "/var/www/${PROJECTFOLDER}"
+	    ServerName local.${PROJECTFOLDER}
 	    <Directory "/var/www/${PROJECTFOLDER}">
 	        AllowOverride All
 	        Require all granted
 	    </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/error-local.{PROJECTFOLDER}.log
+        LogLevel info
+
+        # Let's NOT log some things
+        SetEnvIf Request_URI "^/status.html$" dontlog
+        SetEnvIf Request_URI "^/status.php$" dontlog
+        SetEnvIf Request_URI "^/server-status$" dontlog
+
+        CustomLog ${APACHE_LOG_DIR}/access-www.{PROJECTFOLDER}.log vhost_combined env=!dontlog
 	</VirtualHost>
 EOF
 	)
 	echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
+
+    # vhost for adminer. for greater victory.
+	VHOST=$(cat <<EOF
+	<VirtualHost *:80>
+	    DocumentRoot "/var/www/adminer"
+	    ServerName vagrant.${PROJECTFOLDER}
+	    <Directory "/var/www/adminer">
+	        AllowOverride All
+	        Require all granted
+	    </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/error-local.adminer.log
+        LogLevel info
+
+        # Let's NOT log some things
+        SetEnvIf Request_URI "^/status.html$" dontlog
+        SetEnvIf Request_URI "^/status.php$" dontlog
+        SetEnvIf Request_URI "^/server-status$" dontlog
+
+        CustomLog ${APACHE_LOG_DIR}/access-adminer.net.log vhost_combined env=!dontlog
+	</VirtualHost>
+EOF
+	)
+	echo "${VHOST}" > /etc/apache2/sites-available/001-adminer.conf
+
+
+    # a2ensite /etc/apache2/sites-available/001-adminer.conf ¿error wtf?
+    ln -s /etc/apache2/sites-available/001-adminer.conf /etc/apache2/sites-enabled/001-adminer.conf
+
 	# enable mod_rewrite, mod_expires, mod_headers
 	a2enmod rewrite expires headers
-
-	echo ">>> restarting apache"
-	service apache2 restart
 
 elif [ $HTTPD = 'NGINX' ]; then
 	#statements
@@ -73,6 +111,7 @@ elif [ $HTTPD = 'NGINX' ]; then
 server {
         listen  80 default_server;
         listen [::]:80 default_server ipv6only=on;
+        server_name local.${PROJECTFOLDER}
 
         root /var/www/${PROJECTFOLDER};
         index index.php index.html index.htm;
@@ -101,9 +140,42 @@ EOF
 	)
     echo "${VHOST}" > /etc/nginx/sites-available/default
 
-    service nginx restart
-fi
+    VHOST=$(cat <<EOF
+server {
+        listen  80;
+        listen [::]:80 ipv6only=on;
+        server_name vagrant.${PROJECTFOLDER}
 
+        root /var/www/vagrant;
+        index index.php;
+
+        location / {
+                try_files $uri $uri/ /index.php$is_args$args;
+        }
+
+        error_page 404 /404.html;
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+              root /usr/share/nginx/www;
+        }
+
+        # pass the PHP scripts to FastCGI server listening on the php-fpm socket
+        location ~ \.php$ {
+                try_files \$uri =404;
+                fastcgi_pass unix:/var/run/php5-fpm.sock;
+                fastcgi_index index.php;
+                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+                include fastcgi_params;
+        }
+}
+EOF
+	)
+    echo "${VHOST}" > /etc/nginx/sites-available/adminer.conf
+
+    ln -s /etc/nginx/sites-available/adminer.conf /etc/nginx/sites-enabled/adminer.conf
+
+fi
 
 if [ $DB = 'MYSQL' ]; then
     # install mysql and give password to installer
@@ -131,6 +203,7 @@ echo "CREATE DATABASE IF NOT EXISTS ${PROJECTFOLDER}" | mysql -u root -p$PASSWOR
 if [ $HTTPD = 'APACHE' ]; then
     echo ">>> installing PHP5 (apache)"
     apt-get install -y php5
+
 fi
 
 if [ $HTTPD = 'NGINX' ]; then
@@ -143,7 +216,6 @@ if [ $HTTPD = 'NGINX' ]; then
 	# hoboman runs php as well
 	perl -pi -e 's/((listen\.)?user|group|owner) = www-data/\1 = vagrant/g' /etc/php5/fpm/pool.d/www.conf
 
-	service php5-fpm restart
 fi
 
 echo ">>> installing php extensions"
@@ -167,8 +239,20 @@ EOF
 
 echo "${XDEBUG_INI}" > /etc/php5/mods-available/xdebug.ini
 
+if [ $HTTPD = 'NGINX' ]; then
+    echo ">>> restarting FPM"
+	service php5-fpm restart
+fi
+
+if [ $HTTPD = 'APACHE' ]; then
+    echo ">>> restarting APACHE";
+    service apache2 restart
+fi
+
 # install git
+echo ">>> installing git && svn"
 apt-get -y install git subversion
+
 
 # install Composer
 echo ">>> downloading and installing composer"
@@ -177,7 +261,7 @@ mv composer.phar /usr/local/bin/composer
 
 echo ">>> installing wp-cli"
 # install wp-cli
-curl -Os https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --quiet
 mv wp-cli.phar /usr/local/bin/wp
 chmod 755 /usr/local/bin/wp
 echo ">>> WP-CLI installed"
@@ -188,8 +272,6 @@ phpinfo();
 EOF
 )
 
-service apache2 restart
-
 # nice php info, to have something in place
-echo "${PHPINFO}" > "/var/www/$PROJECT_FOLDER/info.php"
-chown vagrant: "/var/www/$PROJECT_FOLDER/info.php"
+echo "${PHPINFO}" > "/var/www/${PROJECT_FOLDER}/info.php"
+chown vagrant: "/var/www/${PROJECT_FOLDER}/info.php"
